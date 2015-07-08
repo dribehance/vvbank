e37492ec1e578b16e9bab06b17dd7ec60ef5a106
dribehance <dom4j.for.networking@gmail.com>
2015-07-08 11:14:38 +0800

    optional product template

--- src/js/controllers/productController.js
+++ src/js/controllers/productController.js
@@ -4,6 +4,7 @@ var productController = function($scope, $routeParams, toastServices, productSer
     	toastServices.hide();
         if (data.respcode == config.request.SUCCESS) {
             $scope.product = parserServices.parseProduct(data.result);
+            console.log($scope.product)
         } else {
             errorServices.autoHide(data.message)
         }
