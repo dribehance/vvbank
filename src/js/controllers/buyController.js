8947fad53625fe5478d08d64b49c9270ba02bb01
dribehance <dom4j.for.networking@gmail.com>
2015-07-08 10:52:19 +0800

    tokenInterceptor handle reponse error

--- src/js/controllers/buyController.js
+++ src/js/controllers/buyController.js
@@ -1,31 +1,31 @@
-var buyController = function($scope,$location,$routeParams,userServices,productServices,errorServices,config){
-	$scope.input = {
-		amount:"",
-		password:"",
-		agreement:0,
-		remain: $routeParams.remain,
-		balance:0
-	}
-	userServices.info.account().then(function(data){
-		if (data.respcode == config.request.SUCCESS) {
-			$scope.input.balance = data.result.usableAmount;
-		}
-		else {
-			errorServices.autoHide(data.message)
-		}
-	})
-	$scope.ajaxForm = function(form) {
-		productServices.buy({
-			"amount":$scope.input.amount,
-			"password":$scope.input.password,
-			"productId":$routeParams.productID,
-		}).then(function(data){
-			if (data.respcode == config.request.SUCCESS && data.result.status == "1") {
-				$location.path("/index").replace();
-			}
-			else {
-				errorServices.autoHide(data.message)
-			}
-		})
-	}
+var buyController = function($scope, $location, $routeParams, toastServices, userServices, productServices, errorServices, config) {
+    $scope.input = {
+        amount: "",
+        password: "",
+        agreement: 0,
+        remain: $routeParams.remain,
+        balance: 0
+    }
+    userServices.info.account().then(function(data) {
+        if (data.respcode == config.request.SUCCESS) {
+            $scope.input.balance = data.result.usableAmount;
+        } else {
+            errorServices.autoHide(data.message)
+        }
+    })
+    $scope.ajaxForm = function(form) {
+        toastServices.show()
+        productServices.buy({
+            "amount": $scope.input.amount,
+            "password": $scope.input.password,
+            "productId": $routeParams.productID,
+        }).then(function(data) {
+            toastServices.hide()
+            if (data.respcode == config.request.SUCCESS && data.result.status == "1") {
+                $location.path("/index").replace();
+            } else {
+                errorServices.autoHide(data.message)
+            }
+        })
+    }
 }
--- src/js/services/tokenInteractor.js
+++ src/js/services/tokenInteractor.js
@@ -1,4 +1,4 @@
-angular.module("VVBank").factory("tokenInterceptor", function($window, $location, $q, localStorageService, toastServices, config) {
+angular.module("VVBank").factory("tokenInterceptor", function($window, $location, $q, localStorageService, errorServices, config) {
     return {
         // optional method
         'request': function(config) {
@@ -8,6 +8,7 @@ angular.module("VVBank").factory("tokenInterceptor", function($window, $location
 
         // optional method
         'requestError': function(rejection) {
+            // console.log(rejection)
             // do something on error
             // if (canRecover(rejection)) {
             //     return responseOrNewPromise
@@ -22,30 +23,26 @@ angular.module("VVBank").factory("tokenInterceptor", function($window, $location
             // do something on success
             var defer = $q.defer();
             // static response
-            if (response.config.url.indexOf(".html") > 0){
-            	return response;
+            if (response.config.url.indexOf(".html") > 0) {
+                return response;
             }
             // server response
-            if (response.data.respcode == config.request.TOKEN_INVALID ) {
-            	console.log("TOKEN_INVALID")
+            if (response.data.respcode == config.request.TOKEN_INVALID) {
+                console.log("TOKEN_INVALID")
                 localStorageService.remove("token");
                 $location.path("/signIn").replace();
                 return defer.promise;
-            }
-            else {
-            	return response;
+            } else {
+                return response;
             }
             return response;
         },
 
         // optional method
         'responseError': function(rejection) {
-        	console.log("error")
-            // do something on error
-            // if (canRecover(rejection)) {
-            //     return responseOrNewPromise
-            // }
-            // return $q.reject(rejection);
+            var defer = $q.defer();
+            errorServices.requestError(rejection.data,rejection.status,rejection.headers,rejection.config);
+            return defer.promise;
         }
     }
 })
