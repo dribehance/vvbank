d73776ddd8f182b7cbcf04823804ddc29913779d
dribehance <dom4j.for.networking@gmail.com>
2015-08-25 21:45:42 +0800

    fix fastclick bug

--- src/js/controllers/meController.js
+++ src/js/controllers/meController.js
@@ -1,12 +1,12 @@
 var meController = function($scope, $rootScope, errorServices,toastServices, parserServices, userServices, config) {
     // toastServices.show();
-    // userServices.info.account().then(function(data) {
-    //     toastServices.hide();
-    //     if (data.respcode == config.request.SUCCESS) {
-    //         $rootScope.user = angular.extend({},$rootScope.user,parserServices.parseUser(data.result));
-    //     } else {
-    //         toastServices.hide();
-    //         errorServices.autoHide(data.message)
-    //     }
-    // })
+    userServices.info.account().then(function(data) {
+        toastServices.hide();
+        if (data.respcode == config.request.SUCCESS) {
+            $rootScope.user = angular.extend({},$rootScope.user,parserServices.parseUser(data.result));
+        } else {
+            toastServices.hide();
+            errorServices.autoHide(data.message)
+        }
+    })
 }
