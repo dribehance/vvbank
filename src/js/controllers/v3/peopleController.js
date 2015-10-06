9e0705c6f97487a56a325a58ac4148467e76a065
dribehance <dom4j.for.networking@gmail.com>
2015-10-06 14:58:52 +0800

    -

--- src/js/controllers/v3/peopleController.js
+++ src/js/controllers/v3/peopleController.js
@@ -1,4 +1,13 @@
 // by dribehance <dribehance.kksdapp.com>
-var peopleController = function($scope,errorServices,toastServices,localStorageService,config){
-	
+var peopleController = function($scope,peopleServices, errorServices, toastServices, localStorageService, config) {
+	toastServices.show();
+	peopleServices.query().then(function(data){
+		toastServices.hide()
+		if(data.respcode == config.request.SUCCESS){
+			// $scope.peoples = 		
+		}
+		else {
+			errorServices.autoHide(data.message);
+		}
+	})
 }
